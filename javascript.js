dbc2cfbe421a963df39e9b241822714f9ff0d6de
Alan Salganik <asalganik1@gmail.com>
2014-09-15 16:14:05 -0500

    changed header image

--- javascript.js
+++ javascript.js
@@ -1,25 +1,25 @@
 $(document).ready(function() { 
 
-	$.fn.followTo = function (pos) {
-    var $this = this,
-      $window = $(window);
+	// $.fn.followTo = function (pos) {
+ //    var $this = this,
+ //      $window = $(window);
 
-  $window.scroll(function (e) {
-    if ($window.scrollTop() > pos) {
-      $this.css({
-          position: 'absolute',
-          top: pos
-       });
-    } else {
-        $this.css({
-          position: 'fixed',
-          top: 0
-        });
-      }
-    });
-	};
+ //  $window.scroll(function (e) {
+ //    if ($window.scrollTop() > pos) {
+ //      $this.css({
+ //          position: 'absolute',
+ //          top: pos
+ //       });
+ //    } else {
+ //        $this.css({
+ //          position: 'fixed',
+ //          top: 0
+ //        });
+ //      }
+ //    });
+	// };
 
-	$('.name').followTo(710);
+	// $('.name').followTo(710);
 
 
 
